1af81eb0a6fb724f764ad80a9f159c6384fc7c34
hiddartho <56486358+hiddartho@users.noreply.github.com>
2021-08-31 07:00:27 -0700

    chore: updated build tasks

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 var readlineSync = require('readline-sync');
+
+var score=0;
+var username=readlineSync.question("What's your name?");
+
+console.log("Welcome "+username+", DO YOU KNOW Hiddartho?");
+
+//function
+
+function play(question,answer){
+  var userAnswer=readlineSync.question(question);
+  if(userAnswer===answer){
+    console.log("right!");
+    score=score+1;
+  }else{
+    console.log("wrong");
+  }
+  console.log("current score ",score);
+  console.log("--------------------");
+}
+var questions=[{
+  question:"Where do i live?",
+  answer:"dib"
+},
+{
+  question:"What do i Study?",
+  answer:"bca"
+}];
+
+//loop
+var i;
+for(i=0;i<questions.length;i++){
+ play(questions[i].question,questions[i].answer)
+}
+console.log("YAY!, You scored: ",score)
+
+//high scores
+
+var highScore=[{
+  name:"sergio",
+  score:"10"
+},
+{
+  name:"Anukruti",
+  score:"5"
+}];
+
+console.log("The HIGH SCORES are:");
+
+for(var i=0;i<highScore.length;i++){
+  console.log(highScore[i].name,highScore[i].score);
+}
